a5a2b715c1c1364a7a6a786de1b226ba8f75fe86
Rasmus Kaj <kaj@kth.se>
2011-07-17 21:53:57 +0200

    Redirect slashless years to year with trailing slash.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import patterns, url
+from django.shortcuts import redirect
 from blog.views import index, post_detail, tagcloud, tagged
+
+def redirect_year(request, year):
+    return redirect('/%s/' % year)
 
 urlpatterns = patterns(
     '',
     url(r'^$', index, name='index'),
     url(r'^(?P<year>[0-9]{4})/$', index),
+    url(r'^(?P<year>[0-9]{4})$', redirect_year),
     url(r'^(?P<year>[0-9]{4})/(?P<slug>[a-z0-9_-]+)$', post_detail),
     
     url(r'^tag/$', tagcloud),
