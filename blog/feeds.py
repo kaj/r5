5f3950086bfe6810414506ee71b06f5abec826fa
Rasmus Kaj <kaj@kth.se>
2011-08-04 23:33:08 +0200

    Better i18n in feeds.

--- blog/feeds.py
+++ blog/feeds.py
@@ -6,10 +6,11 @@ from django.shortcuts import get_object_or_404
 from taggit.models import Tag
 from blog.models import Post, Update
 from views import filter_by_language
+from django.utils.translation import activate, ugettext_lazy as _
 
 class UpdatesFeed(Feed):
-    title = u'Rasmus.krats.se'
-    subtitle = u'Uppdateringar på Rasmus.krats.se. Skriverier då och då, på webben sedan 1995.'
+    title = _(u'Rasmus.krats.se')
+    subtitle = _(u'Uppdateringar på Rasmus.krats.se. Skriverier då och då, på webben sedan 1995.')
     link = '/'
     author_name = 'Rasmus Kaj'
     author_email = 'rasmus@krats.se'
@@ -20,6 +21,15 @@ class UpdatesFeed(Feed):
     def __init__(self, lang):
         self.lang = lang
         
+    def __call__(self, request, *args, **kwargs):
+        activate(self.lang)
+        return super(UpdatesFeed, self).__call__(request, *args, **kwargs)
+    
+    def get_feed(self, obj, request):
+        feed = super(UpdatesFeed, self).get_feed(obj, request)
+        feed.feed['language'] = self.lang
+        return feed
+
     def items(self):
         updates = Update.objects.all().order_by('-time')[:10]
         return filter_by_language(updates, self.lang)[:5]
@@ -33,7 +43,7 @@ class TaggedUpdatesFeed(UpdatesFeed):
         return get_object_or_404(Tag, slug=tag)
 
     def title(self, obj):
-        return u'Taggat %s på Rasmus.krats.se' % obj
+        return _(u'Taggat %s på Rasmus.krats.se') % obj
 
     def items(self, obj):
         tagged = Post.objects.filter(tags__in=[obj])
