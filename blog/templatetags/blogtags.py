fe60df4ec59d6adfaedfbcb962724f72a6498fd8
Rasmus Kaj <kaj@kth.se>
2011-12-09 21:29:04 +0100

    A first very basic browsing test.

--- blog/templatetags/blogtags.py
+++ blog/templatetags/blogtags.py
@@ -15,13 +15,14 @@ def publine(post):
 @register.inclusion_tag('blog/part_tagcloud.html')
 def tagcloud():
     tags = Tag.objects.all().order_by('name')
-    for tag in tags:
-        tag.n = tag.taggit_taggeditem_items.count()
-    max_n = max(tag.n for tag in tags)
-    exponent = 0.4
-    c = 5.9 / pow(max_n, exponent)
-    for tag in tags:
-        tag.w = int(pow(tag.n, exponent)*c)
+    if tags:
+        for tag in tags:
+            tag.n = tag.taggit_taggeditem_items.count()
+        max_n = max(tag.n for tag in tags)
+        exponent = 0.4
+        c = 5.9 / pow(max_n, exponent)
+        for tag in tags:
+            tag.w = int(pow(tag.n, exponent)*c)
     return {
         'tags': tags,
         }
