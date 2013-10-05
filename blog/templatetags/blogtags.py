8997b0029ffe8f266c3ebf2f415282467e5f7bbf
Rasmus Kaj <kaj@kth.se>
2013-10-05 20:38:35 +0200

    Performance: annotate, no extra queries.

--- blog/templatetags/blogtags.py
+++ blog/templatetags/blogtags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from django.contrib.comments.models import Comment
 from taggit.models import Tag
 from math import pow
@@ -15,10 +16,9 @@ def publine(post):
 
 @register.inclusion_tag('blog/part_tagcloud.html')
 def tagcloud():
-    tags = Tag.objects.all().order_by('name')
+    tags = Tag.objects.all().order_by('name').annotate(
+        n = Count('taggit_taggeditem_items'))
     if tags:
-        for tag in tags:
-            tag.n = tag.taggit_taggeditem_items.count()
         max_n = max(tag.n for tag in tags)
         exponent = 0.4
         c = 5.9 / pow(max_n, exponent)
