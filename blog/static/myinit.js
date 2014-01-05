4e993b00ae74d544f17a4777d00684cf98a03dce
Rasmus Kaj <kaj@kth.se>
2014-01-05 02:11:40 +0100

    Get rid of jQuery.
    Everything still works, except for gravatar hovercards.

--- blog/static/myinit.js
+++ blog/static/myinit.js
@@ -1,36 +1,53 @@
-function initSkiplinks(selector) {
-    selector.focus(function() {
-        $('#skiplinks').addClass('withfocus').removeClass('withoutfocus');
+function initSkiplinks() {
+    var sl = document.querySelector('#skiplinks');
+    sl.className = 'withoutfocus';
+    [].forEach.call(sl.querySelectorAll('a'), function(link) {
+	link.addEventListener('focus', function() {
+            sl.className = 'withfocus';
+	});
+	link.addEventListener('blur', function() {
+            sl.className = 'withoutfocus';
+	});
     });
-    selector.blur(function() {
-        $('#skiplinks').removeClass('withfocus').addClass('withoutfocus');
-    });
-}
-function initMyTweetbox() {
-    $('#sideblockwrap').append(
-        '<aside id="tweetbox">'+
-	    '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/rasmus_kaj"  data-widget-id="348079144619356161">Tweets by @rasmus_kaj</a>'+
-	    '<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>');
-    $('#skiplinks ul').append('<li><a href="#tweetbox">my tweets</a></li>');
-    initSkiplinks($('#skiplinks a[href=#tweetbox]'));
-}
-function initMyLibthing() {
-    $('#sideblockwrap').append(
-        '<aside id="booksbox"><h1>Några bra böcker</h1>'
-	+'<div id="w5c54f5e485d879152955168d893d33ab"></div>'
-	+'<script type="text/javascript" charset="UTF-8" src="'
-	+'http://www.librarything.com/widget_get.php?userid=kaj&amp;'
-	+'theID=w5c54f5e485d879152955168d893d33ab"></script></aside>');
-    $('#skiplinks ul').append('<li><a href="#booksbox">några bra böcker</a></li>');
-    initSkiplinks($('#skiplinks a[href=#booksbox]'));
 }
 
-$(document).ready(function() {
-    $('#skiplinks').addClass('withoutfocus');
-    initSkiplinks($('#skiplinks a'));
-    $("figure > a").kratsbox();
-    if ($('#latestcomments').size()) {
+function addscript(d,src,id) {
+    var js,fjs=d.getElementsByTagName('script')[0];
+    if(!d.getElementById(id)){
+	js=d.createElement('script');
+	js.defer='defer';
+	js.async='async';
+	js.id=id;
+	js.src=src;
+	fjs.parentNode.insertBefore(js,fjs);
+    }
+}
+
+function initMyTweetbox() {
+    document.querySelector('#sideblockwrap').innerHTML +=
+        ('<aside id="tweetbox">'+
+	    '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/rasmus_kaj"  data-widget-id="348079144619356161">Tweets by @rasmus_kaj</a>');
+    document.querySelector('#skiplinks ul').innerHTML +=
+    '<li><a href="#tweetbox">my tweets</a></li>';
+    addscript(document,'http://platform.twitter.com/widgets.js',
+	      'twitter-wjs')
+}
+function initMyLibthing() {
+    document.querySelector('#sideblockwrap').innerHTML +=
+       ('<aside id="booksbox"><h1>Några bra böcker</h1>'
+	+'<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
+    theDestination='w5c54f5e485d879152955168d893d33ab';
+    addscript(document,'http://www.librarything.com/widget_get.php?'+
+	      'userid=kaj&theID=w5c54f5e485d879152955168d893d33ab',
+	      'libthing-wjs')
+    document.querySelector('#skiplinks ul').innerHTML += '<li><a href="#booksbox">några bra böcker</a></li>';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (document.querySelector('#latestcomments')) {
         initMyTweetbox();
         initMyLibthing();
     }
-});
+    initSkiplinks();
+    kratsbox("figure > a");
+}, false);
