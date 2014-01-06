463aefc4eeee5f9eef88ba40c2930e060f6f91f2
Rasmus Kaj <kaj@kth.se>
2014-01-06 08:10:15 +0100

    Cleanup my "initboxes".

--- blog/static/myinit.js
+++ blog/static/myinit.js
@@ -14,36 +14,34 @@ if ('querySelector' in document && 'addEventListener' in window &&
         });
     }
     
-    function addscript(d,src,id) {
-        var js,fjs=d.getElementsByTagName('script')[0];
-        if(!d.getElementById(id)){
-            js=d.createElement('script');
-            js.defer='defer';
-            js.async='async';
-            js.id=id;
-            js.src=src;
-            fjs.parentNode.insertBefore(js,fjs);
+    function addscript(src,id) {
+        if (!document.getElementById(id)) {
+            var e=document.createElement('script');
+            e.defer=e.async=true;
+            e.id=id;
+            e.src=src;
+            document.querySelector('head').appendChild(e);
         }
     }
     
     function initMyTweetbox() {
         document.querySelector('#sideblockwrap').innerHTML +=
         ('<aside id="tweetbox">'+
-         '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/rasmus_kaj"  data-widget-id="348079144619356161">Tweets by @rasmus_kaj</a>');
+         '<a class="twitter-timeline" href="https://twitter.com/rasmus_kaj"'+
+         ' data-dnt="true"  data-widget-id="348079144619356161">'+
+         'Tweets by @rasmus_kaj</a>');
+        addscript('http://platform.twitter.com/widgets.js', 'twitter-wjs')
         document.querySelector('#skiplinks ul').innerHTML +=
         '<li><a href="#tweetbox">my tweets</a></li>';
-        addscript(document,'http://platform.twitter.com/widgets.js',
-                  'twitter-wjs')
     }
     function initMyLibthing() {
         document.querySelector('#sideblockwrap').innerHTML +=
         ('<aside id="booksbox"><h1>Några bra böcker</h1>'
          +'<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
-        theDestination='w5c54f5e485d879152955168d893d33ab';
-        addscript(document,'http://www.librarything.com/widget_get.php?'+
-                  'userid=kaj&theID=w5c54f5e485d879152955168d893d33ab',
-                  'libthing-wjs')
-        document.querySelector('#skiplinks ul').innerHTML += '<li><a href="#booksbox">några bra böcker</a></li>';
+        addscript('http://www.librarything.com/widget_get.php?userid=kaj'+
+                  '&theID=w5c54f5e485d879152955168d893d33ab', 'libthing-wjs')
+        document.querySelector('#skiplinks ul').innerHTML +=
+        '<li><a href="#booksbox">några bra böcker</a></li>';
     }
     
     document.addEventListener('DOMContentLoaded', function() {
