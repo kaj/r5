1457e7c62e996ab8def31eb297a7cde2756bc0ad
Rasmus Kaj <kaj@kth.se>
2014-01-06 08:51:51 +0100

    Optimize js for small size.  :-)

--- blog/static/myinit.js
+++ blog/static/myinit.js
@@ -1,8 +1,8 @@
-if ('querySelector' in document && 'addEventListener' in window &&
-    'forEach' in Array) {
+!function(d){if ('querySelector' in d && 'addEventListener' in window &&
+                 'forEach' in Array) {
 
     function initSkiplinks() {
-        var sl = document.querySelector('#skiplinks');
+        var sl = d.getElementById('skiplinks');
         sl.className = 'withoutfocus';
         Array.prototype.forEach.call(sl.querySelectorAll('a'), function(link) {
             link.addEventListener('focus', function() {
@@ -15,41 +15,32 @@ if ('querySelector' in document && 'addEventListener' in window &&
     }
     
     function addscript(src,id) {
-        if (!document.getElementById(id)) {
-            var e=document.createElement('script');
+        if (!d.getElementById(id)) {
+            var e=d.createElement('script');
             e.defer=e.async=true;
             e.id=id;
             e.src=src;
-            document.querySelector('head').appendChild(e);
+            d.querySelector('head').appendChild(e);
         }
     }
     
-    function initMyTweetbox() {
-        document.querySelector('#sideblockwrap').innerHTML +=
-        ('<aside id="tweetbox">'+
-         '<a class="twitter-timeline" href="https://twitter.com/rasmus_kaj"'+
-         ' data-dnt="true"  data-widget-id="348079144619356161">'+
-         'Tweets by @rasmus_kaj</a>');
-        addscript('http://platform.twitter.com/widgets.js', 'twitter-wjs')
-        document.querySelector('#skiplinks ul').innerHTML +=
-        '<li><a href="#tweetbox">my tweets</a></li>';
-    }
-    function initMyLibthing() {
-        document.querySelector('#sideblockwrap').innerHTML +=
-        ('<aside id="booksbox"><h1>Några bra böcker</h1>'
-         +'<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
-        addscript('http://www.librarything.com/widget_get.php?userid=kaj'+
-                  '&theID=w5c54f5e485d879152955168d893d33ab', 'libthing-wjs')
-        document.querySelector('#skiplinks ul').innerHTML +=
-        '<li><a href="#booksbox">några bra böcker</a></li>';
-    }
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        if (document.querySelector('#latestcomments')) {
-            initMyTweetbox();
-            initMyLibthing();
+    d.addEventListener('DOMContentLoaded', function() {
+        if (d.getElementById('latestcomments')) {
+            d.getElementById('sideblockwrap').innerHTML +=
+            ('<aside id="tweetbox">'+
+             '<a class="twitter-timeline" href="https://twitter.com/rasmus_kaj"'+
+             ' data-dnt="true"  data-widget-id="348079144619356161">'+
+             'Tweets by @rasmus_kaj</a></aside>'+
+             '<aside id="booksbox"><h1>Några bra böcker</h1>'+
+             '<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
+            d.querySelector('#skiplinks ul').innerHTML +=
+            ('<li><a href="#tweetbox">my tweets</a></li> '+
+             '<li><a href="#booksbox">några bra böcker</a></li>');
+            addscript('http://platform.twitter.com/widgets.js', 'twitter-wjs')
+            addscript('http://www.librarything.com/widget_get.php?userid=kaj'+
+                      '&theID=w5c54f5e485d879152955168d893d33ab', 'libthing-wjs')
         }
         initSkiplinks();
         kratsbox("figure > a", kbsettings);
     }, false);
-}
+}}(document);
