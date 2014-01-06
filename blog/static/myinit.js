5fa9e8804a8611833c6373f09f7b0087a052bbee
Rasmus Kaj <kaj@kth.se>
2014-01-06 06:55:46 +0100

    Cut some mustard.

--- blog/static/myinit.js
+++ blog/static/myinit.js
@@ -1,53 +1,57 @@
-function initSkiplinks() {
-    var sl = document.querySelector('#skiplinks');
-    sl.className = 'withoutfocus';
-    [].forEach.call(sl.querySelectorAll('a'), function(link) {
-	link.addEventListener('focus', function() {
-            sl.className = 'withfocus';
-	});
-	link.addEventListener('blur', function() {
-            sl.className = 'withoutfocus';
-	});
-    });
-}
+if ('querySelector' in document && 'addEventListener' in window &&
+    'forEach' in Array) {
 
-function addscript(d,src,id) {
-    var js,fjs=d.getElementsByTagName('script')[0];
-    if(!d.getElementById(id)){
-	js=d.createElement('script');
-	js.defer='defer';
-	js.async='async';
-	js.id=id;
-	js.src=src;
-	fjs.parentNode.insertBefore(js,fjs);
+    function initSkiplinks() {
+        var sl = document.querySelector('#skiplinks');
+        sl.className = 'withoutfocus';
+        Array.prototype.forEach.call(sl.querySelectorAll('a'), function(link) {
+            link.addEventListener('focus', function() {
+                sl.className = 'withfocus';
+            });
+            link.addEventListener('blur', function() {
+                sl.className = 'withoutfocus';
+            });
+        });
     }
-}
-
-function initMyTweetbox() {
-    document.querySelector('#sideblockwrap').innerHTML +=
+    
+    function addscript(d,src,id) {
+        var js,fjs=d.getElementsByTagName('script')[0];
+        if(!d.getElementById(id)){
+            js=d.createElement('script');
+            js.defer='defer';
+            js.async='async';
+            js.id=id;
+            js.src=src;
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }
+    
+    function initMyTweetbox() {
+        document.querySelector('#sideblockwrap').innerHTML +=
         ('<aside id="tweetbox">'+
-	    '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/rasmus_kaj"  data-widget-id="348079144619356161">Tweets by @rasmus_kaj</a>');
-    document.querySelector('#skiplinks ul').innerHTML +=
-    '<li><a href="#tweetbox">my tweets</a></li>';
-    addscript(document,'http://platform.twitter.com/widgets.js',
-	      'twitter-wjs')
-}
-function initMyLibthing() {
-    document.querySelector('#sideblockwrap').innerHTML +=
-       ('<aside id="booksbox"><h1>Några bra böcker</h1>'
-	+'<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
-    theDestination='w5c54f5e485d879152955168d893d33ab';
-    addscript(document,'http://www.librarything.com/widget_get.php?'+
-	      'userid=kaj&theID=w5c54f5e485d879152955168d893d33ab',
-	      'libthing-wjs')
-    document.querySelector('#skiplinks ul').innerHTML += '<li><a href="#booksbox">några bra böcker</a></li>';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.querySelector('#latestcomments')) {
-        initMyTweetbox();
-        initMyLibthing();
+         '<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/rasmus_kaj"  data-widget-id="348079144619356161">Tweets by @rasmus_kaj</a>');
+        document.querySelector('#skiplinks ul').innerHTML +=
+        '<li><a href="#tweetbox">my tweets</a></li>';
+        addscript(document,'http://platform.twitter.com/widgets.js',
+                  'twitter-wjs')
     }
-    initSkiplinks();
-    kratsbox("figure > a", kbsettings);
-}, false);
+    function initMyLibthing() {
+        document.querySelector('#sideblockwrap').innerHTML +=
+        ('<aside id="booksbox"><h1>Några bra böcker</h1>'
+         +'<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
+        theDestination='w5c54f5e485d879152955168d893d33ab';
+        addscript(document,'http://www.librarything.com/widget_get.php?'+
+                  'userid=kaj&theID=w5c54f5e485d879152955168d893d33ab',
+                  'libthing-wjs')
+        document.querySelector('#skiplinks ul').innerHTML += '<li><a href="#booksbox">några bra böcker</a></li>';
+    }
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        if (document.querySelector('#latestcomments')) {
+            initMyTweetbox();
+            initMyLibthing();
+        }
+        initSkiplinks();
+        kratsbox("figure > a", kbsettings);
+    }, false);
+}
