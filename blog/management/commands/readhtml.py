ea14e596bc31dd621a8ab95b4fe4dbb923695a92
Rasmus Kaj <kaj@kth.se>
2014-02-11 20:46:02 +0100

    Make readfile and d2h methods in Command.

--- blog/management/commands/readhtml.py
+++ blog/management/commands/readhtml.py
@@ -51,114 +51,114 @@ class Command(NoArgsCommand):
                 if fn.endswith('.html') and not fn.startswith('.#'):
                     filename = os.path.join(root, fn)
                     try:
-                        readfile(filename)
+                        self.readfile(filename)
                     except Exception as err:
                         print "Failed to load %s" % filename
                         raise err
 
-def readfile(filename):
-    print "Handle", filename
-    slug = os.path.basename(filename)[:-5]
-    dirname = os.path.dirname(filename)
-    #if slug in ['index', 'sv', 'en']:
-    #    slug = os.path.basename(os.path.dirname(filename))
-    if slug.endswith('.sv') or slug.endswith('.en'):
-        slug = slug[:-3]
-    else:
-        print "Strange, language code missing in filename %s" % filename
-    
-    for prefix, url in nsmap.items():
-        ElementTree.register_namespace(prefix, url)
+    def readfile(self, filename):
+        print "Handle", filename
+        slug = os.path.basename(filename)[:-5]
+        dirname = os.path.dirname(filename)
+        #if slug in ['index', 'sv', 'en']:
+        #    slug = os.path.basename(os.path.dirname(filename))
+        if slug.endswith('.sv') or slug.endswith('.en'):
+            slug = slug[:-3]
+        else:
+            print "Strange, language code missing in filename %s" % filename
 
-    tree = ElementTree.parse(filename)
-    # Ok, I don't know if this is a good idea, but mark parents.
-    # Maybe there is a flag to ElementTree that provides this instead?
-    for e in tree.iter():
-        for child in list(e):
-            child.parent = e
-    
-    date = parsedate(serialize(tree.find('head/pubdate', nsmap)))
-    if not date:
-        print "Print ignoring %s, pubdate missing." % filename
-        return
-    
-    lang = tree.getroot().get('lang')
-    p, isnew = Post.objects.get_or_create(posted_time__year=date.year,
-                                          slug=slug,
-                                          lang=lang,
-                                          defaults={'posted_time': date})
+        for prefix, url in nsmap.items():
+            ElementTree.register_namespace(prefix, url)
 
-    tags = [textcontent(e) for e in (tree.getroot().findall('head/tag', nsmap) or [])]
-    if tags:
-        p.tags.add(*tags)
-    
-    # Create an empty update for original posting
-    Update.objects.filter(post=p).delete()
-    update, isnew = Update.objects.get_or_create(post=p, note='',
-                                                 defaults={'time': date})
-    if not isnew:
-        update.time=date
-        update.save()
-    for revision in tree.findall('./head/update', nsmap):
-        rdate = parsedate(revision.get('date'))
-        note = d2h(revision)
-        # print rdate, note[:40]
-        update, isnew = Update.objects.get_or_create(post=p, time=rdate)
-        update.note = note
-        update.save()
-    
-    p.posted_time = date
-    p.title = d2h(tree.find('head/title', nsmap))
-    p.abstract = d2h(tree.find('head/abstract', nsmap))
-    p.content = d2h(tree.getroot(), dirname, str(date.year) if date else '')
-    for image in tree.findall('.//figure'):
-        if 'front' in image.get('class', ''):
-            del image.attrib['class']
-            p.frontimage = serialize(image, False)
-    p.save()
-    # print " ", date, slug, p.title, tags
+        tree = ElementTree.parse(filename)
+        # Ok, I don't know if this is a good idea, but mark parents.
+        # Maybe there is a flag to ElementTree that provides this instead?
+        for e in tree.iter():
+            for child in list(e):
+                child.parent = e
 
-def redirect(old_path, new_path):
-    if old_path == new_path:
-        return
-    redirect, _isnew = Redirect.objects.get_or_create(
-        site_id=settings.SITE_ID,
-        old_path=old_path)
-    redirect.new_path = new_path
-    redirect.save();
+        date = parsedate(serialize(tree.find('head/pubdate', nsmap)))
+        if not date:
+            print "Print ignoring %s, pubdate missing." % filename
+            return
 
-def d2h(elem, dirname='', year=''):
-    if elem is None:
-        return ''
+        lang = tree.getroot().get('lang')
+        p, isnew = Post.objects.get_or_create(posted_time__year=date.year,
+                                              slug=slug,
+                                              lang=lang,
+                                              defaults={'posted_time': date})
 
-    info = elem.find('./head', nsmap)
-    if info is not None:
-        info.clear()
-        info.tag = None
+        tags = [textcontent(e) for e in (tree.getroot().findall('head/tag', nsmap) or [])]
+        if tags:
+            p.tags.add(*tags)
 
-    for e in elem.iter():
-        link = e.get('href')
-        if link:
-            # TODO Figure out a good way to hande media files.
-            pass
-    
-    for e in elem.findall('.//figure', nsmap):
-        ref = e.get('ref')
-        try:
-            e.set('ref', Image.objects.get(sourcename=ref).ref)
-        except Exception as err:
-            mime = { 'JPEG': 'image/jpeg',
-                     'PNG': 'image/png',
-                   }
-            data = PImage.open(os.path.join(settings.IMAGE_FILES_BASE, ref))
-            img = Image(sourcename=ref,
-                        orig_width=data.size[0],
-                        orig_height=data.size[1],
-                        mimetype=mime[data.format])
-            img.save()
-            print 'Found image %s.' % img
-            e.set('ref', img.ref)
-    return serialize(elem)
+        # Create an empty update for original posting
+        Update.objects.filter(post=p).delete()
+        update, isnew = Update.objects.get_or_create(post=p, note='',
+                                                     defaults={'time': date})
+        if not isnew:
+            update.time=date
+            update.save()
+        for revision in tree.findall('./head/update', nsmap):
+            rdate = parsedate(revision.get('date'))
+            note = self.d2h(revision)
+            # print rdate, note[:40]
+            update, isnew = Update.objects.get_or_create(post=p, time=rdate)
+            update.note = note
+            update.save()
+
+        p.posted_time = date
+        p.title = self.d2h(tree.find('head/title', nsmap))
+        p.abstract = self.d2h(tree.find('head/abstract', nsmap))
+        p.content = self.d2h(tree.getroot(), dirname, str(date.year) if date else '')
+        for image in tree.findall('.//figure'):
+            if 'front' in image.get('class', ''):
+                del image.attrib['class']
+                p.frontimage = serialize(image, False)
+        p.save()
+        # print " ", date, slug, p.title, tags
+
+    def redirect(old_path, new_path):
+        if old_path == new_path:
+            return
+        redirect, _isnew = Redirect.objects.get_or_create(
+            site_id=settings.SITE_ID,
+            old_path=old_path)
+        redirect.new_path = new_path
+        redirect.save();
+
+    def d2h(self, elem, dirname='', year=''):
+        if elem is None:
+            return ''
+
+        info = elem.find('./head', nsmap)
+        if info is not None:
+            info.clear()
+            info.tag = None
+
+        for e in elem.iter():
+            link = e.get('href')
+            if link:
+                # TODO Figure out a good way to hande media files.
+                pass
+
+        for e in elem.findall('.//figure', nsmap):
+            ref = e.get('ref')
+            try:
+                e.set('ref', Image.objects.get(sourcename=ref).ref)
+            except Exception as err:
+                mime = { 'JPEG': 'image/jpeg',
+                         'PNG': 'image/png',
+                       }
+                data = PImage.open(os.path.join(settings.IMAGE_FILES_BASE, ref))
+                img = Image(sourcename=ref,
+                            orig_width=data.size[0],
+                            orig_height=data.size[1],
+                            mimetype=mime[data.format])
+                img.save()
+                print 'Found image %s.' % img
+                e.set('ref', img.ref)
+        return serialize(elem)
 
 def serialize(elem, skip_root=True):
     if elem is None:
@@ -178,5 +178,5 @@ def serialize(elem, skip_root=True):
 def textcontent(e):
     if e is None:
         return ''
-    
+
     return (e.text or '') + u''.join(textcontent(ee)+(ee.tail or '') for ee in list(e))
