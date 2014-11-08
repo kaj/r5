e99a2347d10f3b19107465840759f8e34d40475b
Rasmus Kaj <kaj@kth.se>
2014-11-08 23:29:07 +0100

    Add a missing import.

--- r5comments/forms.py
+++ r5comments/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 from django.contrib.comments.forms import CommentForm
 from django.contrib.comments.models import Comment
 from django.utils.translation import ugettext_lazy as _
