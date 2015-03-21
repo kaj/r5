8db26ad575ad4f7810d1e458a67e0f6f4d256770
Rasmus Kaj <kaj@kth.se>
2015-03-22 00:04:06 +0100

    More efficient DOM manipulation.

--- blog/static/myinit.js
+++ blog/static/myinit.js
@@ -36,15 +36,15 @@
     }
     function rkinit() {
         if (d.getElementById('latestcomments')) {
-            d.getElementById('sideblockwrap').innerHTML +=
-            ('<aside id="tweetbox">'+
+            d.getElementById('sideblockwrap').insertAdjacentHTML('beforeend',
+             '<aside id="tweetbox">'+
              '<a class="twitter-timeline" href="https://twitter.com/rasmus_kaj"'+
              ' data-dnt="true" data-widget-id="348079144619356161">'+
              'Tweets by @rasmus_kaj</a></aside>'+
              '<aside id="booksbox"><h1>Några bra böcker</h1>'+
              '<div id="w5c54f5e485d879152955168d893d33ab"></div></aside>');
-            d.querySelector('#skiplinks ul').innerHTML +=
-            ('<li><a href="#tweetbox">my tweets</a></li> '+
+            d.querySelector('#skiplinks ul').insertAdjacentHTML('beforeend',
+             '<li><a href="#tweetbox">my tweets</a></li> '+
              '<li><a href="#booksbox">några bra böcker</a></li>');
             addscript('http://platform.twitter.com/widgets.js', 'twitter-wjs')
             addscript('http://www.librarything.com/widget_get.php?userid=kaj'+
