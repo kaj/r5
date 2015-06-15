716bab7d05a6084871194c96349e9f84e09d8df8
Rasmus Kaj <kaj@kth.se>
2015-06-15 06:52:26 +0200

    Post with only nonpublic comments was lost.
    Maybe not the most efficient database fix, but also some tests,
    so the query can be optimized later.

--- blog/tests/test_browse.py
+++ blog/tests/test_browse.py
@@ -10,7 +10,7 @@ import html5lib
 from html5lib import treebuilders
 from lxml import etree
 from blog.models import Post, Update
-from datetime import datetime
+from datetime import datetime, time
 from re import sub
 from r5comments.models import Comment
 
@@ -20,19 +20,46 @@ def select_texts(doc, selector):
                                with_tail=False))
             for e in CSSSelector(selector)(doc)]
 
+def createPost(title, content, lang='sv', abstract='', tags=None,
+               posted_time=None):
+    post, _ = Post.objects.get_or_create(
+        title=title, lang=lang, posted_time=posted_time,
+        abstract=abstract, content=content)
+    Update.objects.get_or_create(post=post, time=post.posted_time)
+    if tags:
+        post.tags.add(*tags)
+
+    class PostWrapper:
+        def __init__(self, post):
+            self.post = post
+
+        def addComment(self, by_name, by_email, comment, submit_date,
+                       is_public=True, is_removed=False):
+            c, _ = Comment.objects.get_or_create(
+                post=self.post, by_name=by_name, by_email=by_email, comment=comment,
+                is_public=is_public, is_removed=is_removed)
+            if isinstance(submit_date, time):
+                c.submit_date=datetime.combine(self.post.posted_time,
+                                               submit_date)
+            else:
+                c.submit_date=submit_date
+            c.is_public=is_public
+            c.is_removed=is_removed
+            c.save()
+
+    return PostWrapper(post)
+
 class BlogTestCase(TestCase):
     
     def setUp(self):
         self.c = Client()
-        p, _ = Post.objects.get_or_create(
-            title='Foo',
-            posted_time= datetime(2013, 11, 5,  13, 47),
-            lang = 'sv',
-            abstract = '<p>Lorem ipsum dolor</p>\n',
-            content='<p>En text som i princip.</p>\n' +
-            '<p>Skulle kunna vara ganska intressant.</p>\n')
-        Update.objects.get_or_create(post=p, time=p.posted_time)
-        p.tags.add('junk', 'test')
+        createPost(title='Foo',
+                   posted_time= datetime(2013, 11, 5,  13, 47),
+                   abstract = '<p>Lorem ipsum dolor</p>\n',
+                   content='<p>En text som i princip.</p>\n' +
+                   '<p>Skulle kunna vara ganska intressant.</p>\n',
+                   tags=['junk', 'test'])
+
     def get(self, url, expected_status_code=200, expected_location=''):
         response = self.c.get(url)
         self.assertEqual((expected_status_code, expected_location, 
@@ -122,45 +149,36 @@ class SimpleTest(BlogTestCase):
 class CommentedTest(BlogTestCase):
     def setUp(self):
         self.c = Client()
-        p1, _ = Post.objects.get_or_create(
-            title = 'Foo', lang = 'sv',
-            posted_time = datetime(2013, 11, 5,  13, 47),
-            abstract = '<p>Lorem ipsum dolor</p>\n',
-            content='<p>En text som i princip.</p>\n' +
-            '<p>Skulle kunna vara ganska intressant.</p>\n')
-        Update.objects.get_or_create(post=p1, time=p1.posted_time)
-        Comment.objects.get_or_create(
-            post=p1, by_name='X', by_email='a@b', comment='Hejsan',
-            submit_date=datetime(2013, 11, 6, 11, 12),
-            is_public=True)
-        p1.tags.add('junk', 'test')
-        p2, _ = Post.objects.get_or_create(
-            title = 'Bar', lang = 'sv',
-            posted_time = datetime(2015, 6, 14,  18, 32),
-            content='<p>En kort text.</p>\n')
-        Update.objects.get_or_create(post=p2, time=p2.posted_time)
-        p2.tags.add('test')
-        Comment.objects.get_or_create(
-            post=p2, by_name='X', by_email='a@b', comment='Hejsan',
-            submit_date=datetime(2015, 6, 14, 18, 36),
-            is_public=True)
-        Comment.objects.get_or_create(
-            post=p2, by_name='Y', by_email='a@b', comment='Hejsan',
-            submit_date=datetime(2015, 6, 14, 18, 17),
-            is_public=True)
-        Comment.objects.get_or_create(
-            post=p2, by_name='Z', by_email='a@z', comment='Spam',
-            submit_date=datetime(2015, 6, 14, 18, 18),
-            is_removed=True)
-        Comment.objects.get_or_create(
-            post=p2, by_name='Z', by_email='a@z', comment='Spam',
-            submit_date=datetime(2015, 6, 14, 18, 18))
-        p3, _ = Post.objects.get_or_create(
-            title = 'Baz', lang = 'sv',
-            posted_time = datetime(2015, 6, 14,  18, 34),
-            content='<p>En till kort text.</p>\n')
-        Update.objects.get_or_create(post=p3, time=p3.posted_time)
-        p3.tags.add('test')
+        p1 = createPost(title = 'Foo',
+                        posted_time = datetime(2013, 11, 5,  13, 47),
+                        abstract = '<p>Lorem ipsum dolor</p>\n',
+                        content='<p>En text som i princip.</p>\n' +
+                        '<p>Skulle kunna vara ganska intressant.</p>\n',
+                        tags = ['junk', 'test'])
+        p1.addComment('Nisse', 'nisse@se', comment='Hejsan',
+                      submit_date=datetime(2013, 11, 6, 11, 12),
+                      is_public=True)
+        p2 = createPost(title = 'Bar',
+                        posted_time = datetime(2015, 6, 14,  18, 32),
+                        content='<p>En kort text.</p>\n',
+                        tags=['test'])
+        p2.addComment('Nisse', 'nisse@se', comment='Hejsan',
+                      submit_date=time(18, 36), is_public=True)
+        p2.addComment('Kalle', 'kalle@com', comment='Hejsan',
+                      submit_date=time(18, 17), is_public=True)
+        p2.addComment('Spammer', 'a@xx', comment='Spam',
+                      submit_date=time(18, 18),
+                      is_removed=True)
+        p2.addComment('Porn', 'p@xx', comment='Spam',
+                      submit_date=time(18, 18), is_public=False)
+        p3 = createPost(title = 'Baz',
+                        posted_time = datetime(2015, 6, 14,  18, 34),
+                        abstract='<p>Blahonga.</p>',
+                        content='<p>En till kort text.</p>\n',
+                        tags=['test'])
+        p3.addComment('Spammer', 'a@xx', comment='Spam',
+                      submit_date=time(18, 38),
+                      is_public=False, is_removed=True)
 
     def test_get_frontpage(self):
         doc = self.get('/sv')
@@ -171,7 +189,30 @@ class CommentedTest(BlogTestCase):
                          select_texts(doc, 'header #sitename'))
         self.assertEqual(['Baz', 'Bar', 'Foo'],
                          select_texts(doc, 'article h1'))
-        self.assertEqual([u'Kommentera inlägget',
+        self.assertEqual([u'Läs och kommentera inlägget Baz',
                           u'Läs 2 kommentarer',
                           u'Läs inlägget Foo och en kommentar'],
                          select_texts(doc, 'article p.readmore'))
+
+    def test_get_baz(self):
+        '''A post with no public comments.'''
+        doc = self.get('/2015/baz.sv')
+        self.assertEqual(['Baz', 'Skriv en kommentar'],
+                         select_texts(doc, 'main h1'))
+
+    def test_get_bar(self):
+        '''A post with two public comments.'''
+        doc = self.get('/2015/bar.sv')
+        self.assertEqual(['Bar', 'Kommentarer',
+                          'Kalle, 2015-06-14 18:17',
+                          'Nisse, 2015-06-14 18:36',
+                          'Skriv en kommentar'],
+                         select_texts(doc, 'main h1'))
+
+    def test_get_foo(self):
+        '''A post with one public comments.'''
+        doc = self.get('/2013/foo.sv')
+        self.assertEqual(['Foo', 'Kommentarer',
+                          'Nisse, 2013-11-06 11:12',
+                          'Skriv en kommentar'],
+                         select_texts(doc, 'main h1'))
